7bb7ada8408eb2b5d58966420278f8db44f1f863
QuimSalinas <salinas.volta2@gmail.com>
2020-07-16 10:26:23 +0200

    Updates

---
+++ precache-manifest.f1dd713ab78f493b5df8bb58023cd539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ee5cc17c5ab28e90bf7bf6af948616",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "d77f5757084babb0ad92",
+    "url": "/react-todo/static/css/main.5151026d.chunk.css"
+  },
+  {
+    "revision": "8612491fceabe6fb726a",
+    "url": "/react-todo/static/js/2.bf8f2a06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.bf8f2a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77f5757084babb0ad92",
+    "url": "/react-todo/static/js/main.d87de696.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
